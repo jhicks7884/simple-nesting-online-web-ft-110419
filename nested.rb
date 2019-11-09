5d7b2d940ba8b31dc757d67ce9daa9d4bcad5b2d
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-09 17:45:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,17 +1,18 @@
 
 def hopper
-	programmer_hash =
- 		{
+	programmer_hash = {
         :grace_hopper =>
          {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
+        :alan_kay =>
+         {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
+        :dennis_ritchie => 
+        {
           :known_for => "Unix",
           :languages => ["C"]
         }
