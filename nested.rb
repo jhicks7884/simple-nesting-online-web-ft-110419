1670fa8588fd074f6e207a113b2762641d062cbf
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-09 17:55:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,7 +40,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
+alan_kay_is_known_for
 
 def dennis_ritchies_language
 	programmer_hash =
